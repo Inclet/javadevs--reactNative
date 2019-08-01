b1bb247367136677121d37d2ea342bcfd17f6f49
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-08-01 17:46:11 +0200

    add welcome screen

--- src/Welcome Screen/welcome.test.js
+++ src/Welcome Screen/welcome.test.js
@@ -1,33 +1,29 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Welcome from './index';
+import Welcome from "./index";
 
-describe('Welcome Screen', () => {
-  describe('Rendering', () => {
-    it('Should match Snapshot', () => {
-      const component = shallow(<Welcome />);
-      expect(component).toMatchSnapshot();
-    });
-
-    it("Should Navigate to About us screen when clicking on an arrow", () => {
-      const props = {
+describe("Welcome Screen", () => {
+  describe("Rendering", () => {
+    let wrapper, props;
+    beforeEach(() => {
+      props = {
         navigation: {
           navigate: jest.fn(),
         },
       };
-      const welcome = shallow(<Welcome {...props} />);
-      welcome.find('[data-testID="nextNavigator"]').simulate("press");
+      wrapper = shallow(<Welcome {...props} />);
+    });
+    it("Should match Snapshot", () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it("Should Navigate to About us screen when clicking on an arrow", () => {
+      wrapper.find('[data-testID="nextNavigator"]').simulate("press");
       expect(props.navigation.navigate).toHaveBeenCalledWith("AboutUs");
     });
 
     it("Should navigate to aboutUs screen when clicking on bottom paginator", () => {
-      const props = {
-        navigation: {
-          navigate: jest.fn(),
-        },
-      };
-      const welcome = shallow(<Welcome {...props} />);
-      welcome.find('[data-testID="bottomNavigator"]').simulate("press");
+      wrapper.find('[data-testID="bottomNavigator"]').simulate("press");
       expect(props.navigation.navigate).toHaveBeenCalledWith("AboutUs");
     });
   });
