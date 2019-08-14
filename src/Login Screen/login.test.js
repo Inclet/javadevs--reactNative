cf4bc16aaa32c14cece8994b814923e1f0415294
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-08-14 21:28:39 +0200

    add return to component wrapper

--- src/Login Screen/login.test.js
+++ src/Login Screen/login.test.js
@@ -1,112 +1,112 @@
-import Login from './index';
-import React from 'react';
-import { googleLogin } from '../../Firebase Socials/googleLogin';
-import { shallow } from "enzyme";
-import openUrlLink from "../../utilities/openUrlLink";
-import { AsyncStorage } from 'react-native';
-jest.mock("react-native-firebase");
-jest.mock("react-native-google-signin");
-jest.mock('firebase');
+import Login from "./index";
+import React from "react";
+import { googleLogin } from "../../Firebase Socials/googleLogin";
+import { shallow } from 'enzyme';
+import openUrlLink from '../../utilities/openUrlLink';
+import { AsyncStorage } from "react-native";
+jest.mock('react-native-firebase');
+jest.mock('react-native-google-signin');
+jest.mock("firebase");
 
 let props, wrapper, navigate;
-describe("Rendering", () => {
+describe('Rendering', () => {
   beforeEach(() => {
     navigate = jest.fn();
     props = {
-      navigation: { navigate }
+      navigation: { navigate },
     };
-    wrapper = shallow(<Login {...props} />);
+    return (wrapper = shallow(<Login {...props} />));
   });
-  it("Should render without crashing", () => {
+  it('Should render without crashing', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
 
-describe("Test User Inputs", () => {
+describe('Test User Inputs', () => {
   beforeEach(() => {
     navigate = jest.fn();
     props = {
-      navigation: { navigate }
+      navigation: { navigate },
     };
-    wrapper = shallow(<Login {...props} />);
+    return (wrapper = shallow(<Login {...props} />));
   });
-  it("Should change state when a user types in email input field", () => {
+  it('Should change state when a user types in email input field', () => {
     wrapper
       .find(`[data-email-input="email"]`)
-      .simulate("changeText", 'user@javadevs.com');
-    expect(wrapper.state().email).toEqual("user@javadevs.com");
+      .simulate('changeText', "user@javadevs.com");
+    expect(wrapper.state().email).toEqual('user@javadevs.com');
   });
 
-  it('Should change state when a user types in password input field', () => {
+  it("Should change state when a user types in password input field", () => {
     wrapper
       .find(`[data-password-input="password"]`)
-      .simulate("changeText", "javadevs19");
-    expect(wrapper.state().password).toEqual("javadevs19");
+      .simulate('changeText', 'javadevs19');
+    expect(wrapper.state().password).toEqual('javadevs19');
   });
 
-  it("should make email field required", () => {
-    wrapper.find(`[data-navigator="buttonNavigator"]`).simulate("press");
-    expect(wrapper.state().errors.email).toEqual("Email is required!");
+  it('should make email field required', () => {
+    wrapper.find(`[data-navigator="buttonNavigator"]`).simulate('press');
+    expect(wrapper.state().errors.email).toEqual('Email is required!');
   });
 
-  it("should make password field required", () => {
+  it('should make password field required', () => {
     wrapper
       .find(`[data-email-input="email"]`)
-      .simulate('changeText', 'user@javadevs.com');
-    wrapper.find(`[data-navigator="buttonNavigator"]`).simulate("press");
+      .simulate("changeText", "user@javadevs.com");
+    wrapper.find(`[data-navigator="buttonNavigator"]`).simulate('press');
 
     expect(wrapper.state().errors.accountError).toEqual(
-      "Password is required!"
+      'Password is required!'
     );
   });
-  it('should make user', async () => {
+  it("should make user", async () => {
     // let component = wrapper.instance();
     // console.log(component);
-    const res = await openUrlLink('yes.com', props.navigation.navigate);
+    const res = await openUrlLink("yes.com", props.navigation.navigate);
     // expct(wrapper).tobeDefined();
   });
-  it("should make signin a user", async () => {
+  it('should make signin a user', async () => {
     wrapper
       .find(`[data-email-input="email"]`)
-      .simulate('changeText', 'user@javadevs.com');
-
-    wrapper
-      .find(`[data-password-input="password"]`)
-      .simulate('changeText', 'javadevs');
-
-    jest.spyOn(AsyncStorage, "setItem");
-
-    await wrapper.find(`[data-navigator="buttonNavigator"]`).simulate('press');
-    await wrapper.update();
-  });
-
-  it("should make return an error when email is not valid", async () => {
-    wrapper.find(`[data-email-input="email"]`).simulate("changeText", "user");
+      .simulate("changeText", "user@javadevs.com");
 
     wrapper
       .find(`[data-password-input="password"]`)
       .simulate("changeText", "javadevs");
 
+    jest.spyOn(AsyncStorage, 'setItem');
+
     await wrapper.find(`[data-navigator="buttonNavigator"]`).simulate("press");
-    // expect(AsyncStorage.setItem).toBeCalled();
     await wrapper.update();
-    expect(wrapper.state().errors.email).toEqual(
-      'The email address is badly formatted'
-    );
   });
 
-  it("should return an error when the account doesn't exist", async () => {
-    wrapper
-      .find(`[data-email-input="email"]`)
-      .simulate('changeText', 'users@javadevs.com');
+  it('should make return an error when email is not valid', async () => {
+    wrapper.find(`[data-email-input="email"]`).simulate('changeText', 'user');
 
     wrapper
       .find(`[data-password-input="password"]`)
-      .simulate('changeText', 'jurassic');
+      .simulate('changeText', 'javadevs');
 
     await wrapper.find(`[data-navigator="buttonNavigator"]`).simulate('press');
     // expect(AsyncStorage.setItem).toBeCalled();
     await wrapper.update();
-    expect(wrapper.state().errors.accountError).toEqual("I am not registered");
+    expect(wrapper.state().errors.email).toEqual(
+      "The email address is badly formatted"
+    );
+  });
+
+  it('should return an error when the account doesn\'t exist', async () => {
+    wrapper
+      .find(`[data-email-input="email"]`)
+      .simulate("changeText", "users@javadevs.com");
+
+    wrapper
+      .find(`[data-password-input="password"]`)
+      .simulate("changeText", "jurassic");
+
+    await wrapper.find(`[data-navigator="buttonNavigator"]`).simulate("press");
+    // expect(AsyncStorage.setItem).toBeCalled();
+    await wrapper.update();
+    expect(wrapper.state().errors.accountError).toEqual('I am not registered');
   });
 });
