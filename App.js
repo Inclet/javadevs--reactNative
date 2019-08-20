c8f7db4ac97aeeb74ba647db7306c8d3d8cc0db0
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-08-20 11:11:39 +0200

    remove commented code

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import Routes from './src/Navigations';
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import { GITHUB_TOKEN } from "react-native-dotenv";
+import React from "react";
+import Routes from "./src/Navigations";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { GITHUB_TOKEN } from 'react-native-dotenv';
 
-// const networkInterface = createNetworkInterface({
-//   uri: 'https://api.github.com/graphql',
-//   headers: {
-//     Authorization: '4acc7a4302a44b71e97eda3d3134ffb53b42ddbe'
-//   }
-// });
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
+  uri: "https://api.github.com/graphql",
   headers: {
-    Authorization: `token ${GITHUB_TOKEN}`
-  }
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
 });
 const App = () => {
-  const prefix = 'javadevsapp://';
+  const prefix = "javadevsapp://";
   return (
     <ApolloProvider client={client}>
       <Routes uriPrefix={prefix} />
